af7dde00f22c5ebb893b89642e37a6445cd0cf98
etienne0114 <etiennetuyihamye@gmail.com>
2025-03-13 11:20:08 +0200

    upgrading

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,13 +3,10 @@ buildscript {
         google()
         mavenCentral()
         gradlePluginPortal()
-        // Add fallback repositories
-        jcenter()
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.0.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22") // Match version in settings.gradle.kts
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
         classpath("com.google.gms:google-services:4.3.15")
     }
 }
@@ -18,8 +15,5 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        // Add fallback repositories
-        jcenter()
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
     }
 }
