f9070adabdf3ca7810d61d66898379b63ec38b3f
etienne0114 <etiennetuyihamye@gmail.com>
2025-03-13 16:03:23 +0200

    udating fixed error

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -12,4 +12,17 @@ dependencies {
     // Remove or replace the unresolved reference
     // implementation(libs.kotlin.gradle.plugin)
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
+}plugins {
+    `kotlin-dsl`
 }
+
+repositories {
+    mavenCentral()
+    google()
+    gradlePluginPortal()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
+}
+
